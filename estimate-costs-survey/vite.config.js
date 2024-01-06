465a60b06b8be16b30735f81300e3403ba5c2134
Katarzyna Skibowska <kat.skibowska@gmail.com>
2024-01-06 10:57:51 +0100

    chunkSizeWarningLimit updated

--- estimate-costs-survey/vite.config.js
+++ estimate-costs-survey/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 16000,
   },
 });
